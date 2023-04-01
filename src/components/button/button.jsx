108e5a358dc9d15c7380b7e50457b3885a677d61
dsdy <dsdy06@gmail.com>
2023-04-01 13:41:34 +0300

    reconfigured button`s style

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,7 +3,7 @@ import "./button.css"
 
 export default function Button() {
   return (
-    <button type="button" className="button">
+    <button type="button" className="button-primary">
       this is button
     </button>
   )
