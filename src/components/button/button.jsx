ed96c0da6d162c7737dcf5d215817ed64e472997
dsdy <dsdy06@gmail.com>
2023-04-02 18:12:11 +0300

    implemented use of props

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,7 +3,7 @@ import "./button.css"
 import propTypes from "prop-types"
 
 export default function Button(props) {
-  const { secondary, classes, text } = props
+  const { secondary, classes, text, onClickFunc } = props
 
   Button.propTypes = {
     secondary: propTypes.bool,
@@ -11,16 +11,29 @@ export default function Button(props) {
     classes: propTypes.string,
 
     text: propTypes.string.isRequired,
+
+    onClickFunc: propTypes.string,
   }
 
   Button.defaultProps = {
     secondary: false,
     classes: null,
+    onClickFunc: null,
   }
 
+  const mainClassName = secondary ? "button-secondary" : "button-primary"
+  const buttonClasses = classes
+    ? [mainClassName, ...classes.split(" ")]
+    : [mainClassName]
+
   return (
-    <button type="button" className="button-primary">
-      this is button
+    <button
+      type="button"
+      onClick={onClickFunc}
+      onKeyDown={onClickFunc}
+      className={buttonClasses.join(" ")}
+    >
+      {text}
     </button>
   )
 }
