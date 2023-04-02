0a847c21eaad53303fbac73ce228f68424451008
dsdy <dsdy06@gmail.com>
2023-04-02 17:58:39 +0300

    added props

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,7 +1,23 @@
 import React from "react"
 import "./button.css"
+import propTypes from "prop-types"
 
-export default function Button() {
+export default function Button(props) {
+  const { secondary, classes, text } = props
+
+  Button.propTypes = {
+    secondary: propTypes.bool,
+
+    classes: propTypes.string,
+
+    text: propTypes.string.isRequired,
+  }
+
+  Button.defaultProps = {
+    secondary: false,
+    classes: null,
+  }
+
   return (
     <button type="button" className="button-primary">
       this is button
