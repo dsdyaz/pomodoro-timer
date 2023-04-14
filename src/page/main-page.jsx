2c274f3a49842fcdfde290bb9a12052a57f2a093
dsdy <dsdy06@gmail.com>
2023-04-14 13:02:10 +0300

    deleted unused imports

--- src/page/main-page.jsx
+++ src/page/main-page.jsx
@@ -1,6 +1,4 @@
 import React from "react"
-import Button from "../components/button/button"
-import Timer from "../components/timer/timer"
 import TimerHandler from "../containers/timer-handler/timer-handler"
 
 export default function Homepage() {
