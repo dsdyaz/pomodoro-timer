3cb25f58412300c89a3bd5da13ac44a29113ede0
dsdy <dsdy06@gmail.com>
2023-04-14 13:01:24 +0300

    deleted unused components on the main page

--- src/page/main-page.jsx
+++ src/page/main-page.jsx
@@ -6,11 +6,7 @@ import TimerHandler from "../containers/timer-handler/timer-handler"
 export default function Homepage() {
   return (
     <div>
-      <Timer minutes="2" seconds="50" />
-      <Timer isGreen minutes="17" seconds="48" />
       <TimerHandler />
-      <Button text="thats button" />
-      <Button text="secondary" secondary />
     </div>
   )
 }
