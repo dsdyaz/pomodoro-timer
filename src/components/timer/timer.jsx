52f9363bb80d5af66f34e4614826f3de89b05352
dsdy <dsdy06@gmail.com>
2023-05-07 18:14:49 +0300

    work in progress

--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -1,23 +1,40 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import propTypes from "prop-types"
 import "./timer.css"
+import { format } from "fecha"
 
 export default function Timer(props) {
   const { time, isGreen } = props
 
   Timer.propTypes = {
-    time: propTypes.string.isRequired,
+    time: propTypes.number,
 
     isGreen: propTypes.bool,
   }
 
   Timer.defaultProps = {
+    time: 0,
     isGreen: false,
   }
   const classes = isGreen ? "timer timer-green" : "timer"
+
+  const [timer, setTimer] = useState(time)
+  console.log(time)
+  const decreaseTimer = () => {
+    setTimer(timer - 1000)
+  }
+
+  useEffect(() => {
+    if (timer <= 0) {
+      return undefined
+    }
+    const countdown = setInterval(decreaseTimer, 1000)
+    return () => clearInterval(countdown)
+  }, [decreaseTimer, timer])
+
   return (
     <div className={classes}>
-      <span>{time}</span>
+      <span>{format(timer, "mm:ss")}</span>
     </div>
   )
 }
--- src/containers/timer-handler/timer-handler.jsx
+++ src/containers/timer-handler/timer-handler.jsx
@@ -15,38 +15,37 @@ export default function TimerHandler() {
   const timerId = useSelector(SelectTimerId)
   const remainingTime = useSelector(SelectRemainingTime)
   let interval
-  const [time, setTime] = useState(100000)
 
   if (remainingTime > 0 && remainingTime <= 1000) {
     dispatch({ type: "timer/stop" })
     dispatch({ type: "timer/recordTime", payload: 0 })
     dispatch({ type: "timer/toggleRest" })
   }
-
-  const decreaseTimer = () => {
-    setTime(time - 1000)
-  }
-
+  const [time, setTime] = useState(0)
   useEffect(() => {
-    if (time <= 0) {
-      return
+    if (isRunning) {
+      setTime(6000)
+    } else {
+      setTime(0)
     }
-    const countdown = setInterval(decreaseTimer, 1000)
-    // eslint-disable-next-line consistent-return
-    return () => clearInterval(countdown)
-  }, [decreaseTimer, time])
+    console.log(time)
+  }, [isRunning])
 
   const runButtonFunc = isRunning
     ? () => {
         console.log("its already going")
       }
-    : () => dispatch({ type: "timer/start" })
+    : () => {
+        dispatch({ type: "timer/start" })
+        setTime(6000)
+      }
 
   return (
     <div className="timer-block">
-      <Timer time={format(time, "mm:ss")} />
+      <Timer time={time} />
       <div className="timer-block__buttons">
         <Button text="run" onClickFunc={() => runButtonFunc()} />
+        <Button text={`${time}`} />
         <Button
           text="stop"
           secondary
