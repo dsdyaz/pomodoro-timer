790d9871b3587a844edba68b5db973bbc95788f9
dsdy <dsdy06@gmail.com>
2023-05-08 13:33:59 +0300

    created message component

--- src/page/main-page.jsx
+++ src/page/main-page.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import TimerHandler from "../containers/timer-handler/timer-handler"
 import "./style.css"
+import Message from "../components/message/message"
 
 export default function Homepage() {
   return (
